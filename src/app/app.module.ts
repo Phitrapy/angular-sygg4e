56630c01582f68e50001e8e914ecd4ea269f99b9
Phitrapy <clement.begnaud@gmail.com>
2022-06-09 15:35:39 +0200

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MaterialExampleModule} from '../material.module';
-import {MenuHarnessExample} from './menu-harness-example';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material/core';
-import {HttpClientModule} from '@angular/common/http';
+import { MaterialExampleModule } from '../material.module';
+import { MenuHarnessExample } from './menu-harness-example';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+import { DateRangePickerComponent } from './date-range-picker/date-range-picker.component';
 
 @NgModule({
-  declarations: [MenuHarnessExample],
+  declarations: [MenuHarnessExample, DateRangePickerComponent],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
---
+++ src/app/date-range-picker/date-range-picker.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title Testing with MatMenuHarness
+ */
+@Component({
+  selector: 'custom-range-date-picker',
+  templateUrl: 'date-range-picker.component.html',
+  styleUrls: ['date-range-picker.component.scss'],
+})
+export class DateRangePickerComponent {}
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
